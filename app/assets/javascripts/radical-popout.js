d833d26d92e350c619a77580aec34150db647692
Spoonflower <mhowell@spoonflower.com>
2016-06-30 09:05:43 -0400

    things work

--- app/assets/javascripts/radical-popout.js
+++ app/assets/javascripts/radical-popout.js
@@ -1,19 +1,22 @@
-function clickSet() {
-  circularnav.classList.toggle("closed")
-  circularnav.classList.toggle("clicked");
-  if (circularnav.classList.contains("closed")) {
-    // freshly closed button
-    var pseudoBefore = window.getComputedStyle(
-      document.querySelector('.ss-icon'), ':before'
-    ).animation
-  }
-}
+$(document).ready(function() {
+  var circularnav = document.getElementsByClassName("ss-icon")[0];
 
-var circularnav = document.getElementsByClassName("ss-icon")[0];
-circularnav.addEventListener("click", clickSet, false);
-
-circularnav.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13) {
-    clickSet();
+  function clickSet() {
+    circularnav.classList.toggle("closed")
+    circularnav.classList.toggle("clicked");
+    if (circularnav.classList.contains("closed")) {
+      // freshly closed button
+      var pseudoBefore = window.getComputedStyle(
+        document.querySelector('.ss-icon'), ':before'
+      ).animation
+    }
   }
-});
+
+  circularnav.addEventListener("click", clickSet, false);
+
+  circularnav.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+      clickSet();
+    }
+  });
+})
